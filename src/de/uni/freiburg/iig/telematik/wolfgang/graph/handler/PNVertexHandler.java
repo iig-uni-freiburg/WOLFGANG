412dc18d1d05a74d323056630398cd067d712af7
Parfield <patrick123@gmx.net>
2015-06-25 07:51:04 +0000

    Bugfix for F19: Fixed resizing of objects

--- src/de/uni/freiburg/iig/telematik/wolfgang/graph/handler/PNVertexHandler.java
+++ src/de/uni/freiburg/iig/telematik/wolfgang/graph/handler/PNVertexHandler.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JOptionPane;
+
 import com.mxgraph.model.mxGeometry;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.swing.handler.mxVertexHandler;
@@ -31,54 +33,54 @@ public class PNVertexHandler extends mxVertexHandler {
 	@Override
 	protected void resizeCell(MouseEvent e) {
 		mxGraph graph = graphComponent.getGraph();
-		double scale = graph.getView().getScale();
-
-		Object cell = state.getCell();
-		mxGeometry geometry = graph.getModel().getGeometry(cell);
-
-		if (geometry != null) {
-			double dx = (e.getX() - first.x) / scale;
-			double dy = (e.getY() - first.y) / scale;
-
-			if (isLabel(index)) {
-				geometry = (mxGeometry) geometry.clone();
-
-				if (geometry.getOffset() != null) {
-					dx += geometry.getOffset().getX();
-					dy += geometry.getOffset().getY();
+		if (graph.getSelectionCells().length == 1) {			
+			double scale = graph.getView().getScale();
+	
+			Object cell = state.getCell();
+			mxGeometry geometry = graph.getModel().getGeometry(cell);
+	
+			if (geometry != null) {
+				double dx = (e.getX() - first.x) / scale;
+				double dy = (e.getY() - first.y) / scale;
+	
+				if (isLabel(index)) {
+					geometry = (mxGeometry) geometry.clone();
+	
+					if (geometry.getOffset() != null) {
+						dx += geometry.getOffset().getX();
+						dy += geometry.getOffset().getY();
+					}
+	
+					if (gridEnabledEvent) {
+						dx = graph.snap(dx);
+						dy = graph.snap(dy);
+					}
+	
+					geometry.setOffset(new mxPoint(dx, dy));
+					graph.getModel().setGeometry(cell, geometry);
 				}
-
-				if (gridEnabledEvent) {
-					dx = graph.snap(dx);
-					dy = graph.snap(dy);
+				else {
+					mxRectangle bounds = union(geometry, dx, dy, index);
+					Rectangle rect = bounds.getRectangle();
+	
+					// Snaps new bounds to grid (unscaled)
+					if (gridEnabledEvent) {
+						int x = (int) graph.snap(rect.x);
+						int y = (int) graph.snap(rect.y);
+						rect.width = (int) graph.snap(rect.width - x + rect.x);
+						rect.height = (int) graph.snap(rect.height - y + rect.y);
+						rect.x = x;
+						rect.y = y;
+					}
+					// Checks if resized cells lies in canvas
+					if (rect.getX() >= 0 && rect.getY() >= 0)
+						graph.resizeCell(cell, new mxRectangle(rect));
 				}
-
-				geometry.setOffset(new mxPoint(dx, dy));
-				graph.getModel().setGeometry(cell, geometry);
-			} else {
-
-				// keeps aspect ratio
-				double size = Math.min(dx, dy);
-				dx = size;
-				dy = size;
-				//
-
-				mxRectangle bounds = union(geometry, dx, dy, index);
-				Rectangle rect = bounds.getRectangle();
-
-				// Snaps new bounds to grid (unscaled)
-				if (gridEnabledEvent) {
-					int x = (int) graph.snap(rect.x);
-					int y = (int) graph.snap(rect.y);
-					rect.width = (int) graph.snap(rect.width - x + rect.x);
-					rect.height = (int) graph.snap(rect.height - y + rect.y);
-					rect.x = x;
-					rect.y = y;
-				}
-				// Checks if resized cells lies in canvas
-				if (rect.getX() >= 0 && rect.getY() >= 0)
-					graph.resizeCell(cell, new mxRectangle(rect));
 			}
+		}
+		else {
+			JOptionPane.showMessageDialog(getGraphComponent(), "One object at once allowed for resizing", "Error", JOptionPane.ERROR_MESSAGE);
+			graph.clearSelection();
 		}
 	}
 
