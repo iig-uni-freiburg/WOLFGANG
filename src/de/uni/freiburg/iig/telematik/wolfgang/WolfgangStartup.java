e139c58ef3107c1d8f4ad729507a000dec96edd9
Parfield <patrick123@gmx.net>
2015-07-14 07:00:11 +0000

    Bugfix for running Wolfgang in Windows to enforce consistent layout (setLookAndFeel)

--- src/de/uni/freiburg/iig/telematik/wolfgang/WolfgangStartup.java
+++ src/de/uni/freiburg/iig/telematik/wolfgang/WolfgangStartup.java
@@ -42,6 +42,7 @@ public class WolfgangStartup extends AbstractStartup {
                     tryToOpenNet();
                 }
             } else {
+            	setLookAndFeel();
                 switch (chosenNetType) {
                     case CPN:
                         new WolfgangCPN().setUpGUI();
