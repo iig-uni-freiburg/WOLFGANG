aedf1b6dfdf2c81e024ebe292898f1af075b9b66
rzahoransky <r.zahoransky@gmx.de>
2015-12-01 14:53:59 +0100

    repaired stuff

--- src/de/uni/freiburg/iig/telematik/wolfgang/menu/RTPNToolbar.java
+++ src/de/uni/freiburg/iig/telematik/wolfgang/menu/RTPNToolbar.java
@@ -4,7 +4,7 @@ import java.awt.Dimension;
 
 import javax.swing.JComboBox;
 
-import de.uni.freiburg.iig.telematik.sepia.petrinet.timedNet.concepts.ResourceContext;
+import de.uni.freiburg.iig.telematik.sepia.petrinet.timedNet.concepts.IResourceContext;
 import de.uni.freiburg.iig.telematik.wolfgang.editor.component.PNEditorComponent;
 import de.uni.freiburg.iig.telematik.wolfgang.exception.EditorToolbarException;
 
@@ -25,22 +25,22 @@ public class RTPNToolbar extends PTNetToolBar {
 		repaint();
 	}
 	
-	private JComboBox<ResourceContext> getResourceContextDropBox(){
-		JComboBox<ResourceContext> cmbResult = new JComboBox<>();
+	private JComboBox<IResourceContext> getResourceContextDropBox(){
+		JComboBox<IResourceContext> cmbResult = new JComboBox<>();
 		cmbResult.setSize(150, 30);
 		cmbResult.setPreferredSize(new Dimension(130, 30));
 		return cmbResult;
 	}
 	
-	private JComboBox<ResourceContext> getTimeContextDropBox(){
-		JComboBox<ResourceContext> cmbResult = new JComboBox<>();
+	private JComboBox<IResourceContext> getTimeContextDropBox(){
+		JComboBox<IResourceContext> cmbResult = new JComboBox<>();
 		cmbResult.setSize(150, 30);
 		cmbResult.setPreferredSize(new Dimension(130, 30));
 		return cmbResult;
 	}
 	
-	private JComboBox<ResourceContext> getAccessContextDropBox(){
-		JComboBox<ResourceContext> cmbResult = new JComboBox<>();
+	private JComboBox<IResourceContext> getAccessContextDropBox(){
+		JComboBox<IResourceContext> cmbResult = new JComboBox<>();
 		cmbResult.setSize(150, 30);
 		cmbResult.setPreferredSize(new Dimension(130, 30));
 		return cmbResult;
